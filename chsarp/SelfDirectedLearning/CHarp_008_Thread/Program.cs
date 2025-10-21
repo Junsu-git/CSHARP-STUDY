5b9e357de75f3ab6943df4ad68b14cf56fe3f93e
Junsu-git <wnstn2228@gmail.com>
2025-10-21 15:46:01 +0900

    CHarp_008_Thread 프로젝트 추가 및 스레드 구현
    CHarp_008_Thread 프로젝트 파일을 새로 추가하고, .NET 8.0을 타겟으로 하는 콘솔 애플리케이션으로 설정했습니다.
    Program.cs에 `myBag` 클래스를 추가하여 데이터와 문자열을 저장할 수 있도록 하였으며,
    `myCalculater` 메서드를 통해 비동기적으로 계산을 수행하고 결과를 출력하는 기능을 구현했습니다.
    또한, 솔루션 파일에 `charp_008_Thread` 프로젝트를 추가하여 전체 솔루션에서 관리할 수 있게 하였습니다.

---
+++ chsarp/SelfDirectedLearning/CHarp_008_Thread/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+namespace charp_008_Thread
+{
+    internal class Program
+    {
+        class myBag
+        {
+            public int data;
+            public string str;
+        }
+        static void myCalculater(object data)
+        {
+            myBag myBag = (myBag)data;
+            int sum = myBag.data;
+            sum += 10;
+            Console.WriteLine($"[Thread 2] 번호 []");
+            // 큰 계싼이 필요해... 이 결과가 더 빨리 필요해...
+            Thread.Sleep(8000); // 큰 계산이 있는 것처럼 표현한 것 뿐
+            Console.WriteLine($"[Thread 2] 첫번째 스트링 [{myBag.str}]");
+            //Thread.Sleep(1000); // 큰 계산이 있는 것처럼 표현한 것 뿐
+            Console.WriteLine($"[Thread 2] 조준수 [{sum}]");
+        }
+
+        static void Main(string[] args)
+        {
+            myBag mb = new myBag();
+            mb.data = 49;
+            mb.str = "TEST STRING";
+
+            Thread myThread = new Thread(myCalculater);
+            //Thread myThread2 = new Thread(myCalculater);
+            myThread.IsBackground = false;
+            myThread.Start(mb);
+            //myThread2.Start(mb);
+
+            Int128 sum = 0;
+            Console.WriteLine($"[Thread 1] 번호 [{myThread.ManagedThreadId}]");
+            // ctrl + alt + h0
+            Console.WriteLine($"[Thread 1] 매우 큰 계산 전");
+            //for(Int64 i =0; i < 1_000_000_000; i++)
+            //{
+            //    sum += (Int128)i;
+            //}
+            Thread.Sleep(5_000);
+            Console.WriteLine($"RESULT = [ {sum} ]");
+
+            //Console.ReadLine();
+        }
+    }
+}
