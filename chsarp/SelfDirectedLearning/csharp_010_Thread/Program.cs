386b3d5bac138667434c9a76dc852c2b65cf2a20
Junsu-git <wnstn2228@gmail.com>
2025-11-04 15:38:23 +0900

    새로운 스레드 및 스레드 풀 기능 추가
    C# 프로젝트에 `csharp_010_Thread` 및 `csharp_010_ThreadPool` 두 개의 새로운 프로젝트를 추가했습니다.
    각 프로젝트의 `.csproj` 파일에는 .NET SDK 설정과 실행 파일로 설정하는 속성이 포함되어 있습니다.
    `csharp_010_ThreadPool` 프로젝트는 `ThreadPool.QueueUserWorkItem`을 사용하여 작업을 처리하는 예제를 제공하며,
    `csharp_010_Thread` 프로젝트는 큐를 사용하여 스레드 간의 작업을 관리하고 실행 및 취소를 제어하는 로직을 포함하고 있습니다.
    이로써 멀티스레딩의 활용을 보여주는 다양한 기능이 구현되었습니다.

---
+++ chsarp/SelfDirectedLearning/csharp_010_Thread/Program.cs
@@ -0,0 +1,120 @@
+﻿using System.ComponentModel.Design;
+using System.Runtime.InteropServices;
+
+namespace csharp_010_Thread
+{
+    internal class Program
+    {
+        private static bool _running = false;
+        private static bool _cancleRequest = false;
+
+
+        static void Main(string[] args)
+        {
+            //List<int> radiusList = new List<int>();
+            Queue<int> radiusQueue = new Queue<int>();
+            // Caution Queue Thread = unsafe
+            for (int i = 0; i < 65500; i++)
+            {
+                radiusQueue.Enqueue(i);
+            }
+            Thread t2 = new Thread(Run);
+            ThreadEnable();
+            t2.Start(radiusQueue);
+            Console.WriteLine("Waiting Work Thread cal");
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Enqueue New Data to Work Thread");
+            for (int i = 65501; i <= 70000; i++)
+            {
+                radiusQueue.Enqueue(i);
+            }
+            Console.WriteLine("Waiting Work Thread Last update data cal");
+            Console.WriteLine("============= Aborting Work Thread =============");
+
+            Thread.Sleep(50);
+            Console.WriteLine("============= Disable Work Thread =============");
+            ThreadDisable();
+            Console.WriteLine("============= Cancel Work Thread =============");
+            DoThreadCancel();
+
+            Console.WriteLine("[ MAIN ]Waiting Aborting work Thread in Main");
+        }
+
+        private static void ThreadEnable()
+        {
+            _running = true;
+        }
+        private static void ThreadDisable()
+        {
+            _running = false;
+        }
+        private static void DoThreadCancel()
+        {
+            _cancleRequest = true;
+        }
+        private static void ClearThreadCancel()
+        {
+            _cancleRequest = false;
+        }
+        private static bool IsThreadCancel()
+        {
+            return _cancleRequest;
+        }
+
+        static void Run(object o)
+        {
+            //List<int> list = (List<int>)o;
+            Queue<int> q = (Queue<int>)o;
+            Queue<double> rq = new Queue<double>();
+            //List<double> resultList = new List<double>();
+
+            double result = 0;
+            try
+            {
+                while (_running)
+                {
+                    if (IsThreadCancel())
+                    {
+                        throw new Exception("USER REQUET CANCELLATION");
+                    }
+                    if (q.Count > 0)
+                    {
+                        // huge calc
+                        //foreach (int i in q)
+                        while (q.Count > 0)
+                        {
+                            if (IsThreadCancel())
+                            {
+                                throw new Exception("USER REQUET CANCELLATION");
+                            }
+                            int i = q.Dequeue();
+                            result = i * i * Math.PI;
+                            rq.Enqueue(result);
+                        }
+                        // report move to near by Thread.Sleep()
+
+                    }
+                    else
+                    {
+                        int idx = 0;
+                        while (rq.Count > 0)
+                        {
+                            if (IsThreadCancel())
+                            {
+                                throw new Exception("USER REQUET CANCELLATION");
+                            }
+                            double d = rq.Dequeue();
+                            Console.WriteLine($"[{idx++}] result = {d}");
+                        }
+                        Thread.Sleep(1);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ chsarp/SelfDirectedLearning/csharp_010_ThreadPool/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace csharp_010_ThreadPool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 65_500; i++)
+            {
+                //Thread t1 = new Thread(Run);
+                //t1.Start(i);
+                ThreadPool.QueueUserWorkItem(Run, i);
+            }
+
+            // main 종료되는 것을 방지, Cause Thread pool is Background
+            Console.ReadLine();
+        }
+
+        static void Run(object obj)
+        {
+            int r = (int)obj;
+            double result = r * r * Math.PI;
+            Console.WriteLine($"[ {Thread.CurrentThread.ManagedThreadId} ] radius: {r} result: {result}"); // 예제 편의상 쓰레드 내부에서 출력
+        }
+
+        // 경마 
+        // 청소 루틴
+        // 로그 게임
+        // 
+    }
+}
