0c2d6ebe494f3819111e90c9c55f4b7047340148
Junsu-git <wnstn2228@gmail.com>
2025-11-18 15:35:52 +0900

    Add csharp_012_event project with event example
    Added a new project 'csharp_012_event' to the solution, including an initial Program.cs demonstrating basic event usage and a Temperature class. Updated the solution file to include the new project.

---
+++ chsarp/SelfDirectedLearning/csharp_012_event/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Channels;
+
+namespace csharp_012_event
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myTimer = new System.Timers.Timer();
+            myTimer.Elapsed += (sender, e) => { Console.WriteLine("Events TST with ramda"); };
+
+            myTimer.Interval = 1000;
+            myTimer.Start();
+
+            Console.ReadLine();
+
+            // 온도 객체를 하나 만든다
+            Temperature temp = new Temperature()
+            {
+                MaxTemperature = 80.0,
+                MinTemperature = 10.0
+            };
+
+            
+        }
+    }
+    class Temperature
+    {
+        private float currentT;
+        public double MaxTemperature { get; set; }
+        public double MinTemperature { get; set; }
+        
+        public event EventHandler
+        public float GetTemperature()
+        {
+            return currentT;
+        }
+        public bool IsExceedMaxTemp()
+        {
+            return false;
+        }
+        
+    }
+
+    public class TemperatueEventArgs
+    {
+        public double Temperature { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
